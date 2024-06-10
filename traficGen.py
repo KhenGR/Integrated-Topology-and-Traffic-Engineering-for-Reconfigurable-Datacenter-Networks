fc67652bccd1f4cbf53a9290ca7bfccb52aac962
KhenGR <30404840+KhenGR@users.noreply.github.com>
2024-06-10 17:49:24 +0300

    Made traffic generator

---
+++ traficGen.py
@@ -0,0 +1,51 @@
+from numpy import random
+import numpy as np
+import scipy as sp
+import time
+def traffic_generator(nl, ns, cl, n, sigma):
+    cs = 1 - cl
+    large_ratio = cl / nl
+    if nl > 0 and cl > 0:
+        large_flows = np.random.normal(large_ratio, sigma*(large_ratio), nl)
+    else:
+        large_flows = np.zeros(nl)
+    if nl > 0 and cl > 0:
+        small_flows = np.random.normal(cs / ns, sigma*(cs / ns), ns)
+    else:
+        small_flows = np.zeros(ns)
+    largeFlowsMatrix = np.zeros((n, n))
+    for i in range(nl):
+        largeFlowsMatrix += large_flows[i]*random_permutation_matrix_no_dig(n)
+    small_flows_matrix = np.zeros((n, n))
+    for i in range(ns):
+        small_flows_matrix += small_flows[i]*random_permutation_matrix_no_dig(n)
+    new_matrix = small_flows_matrix+largeFlowsMatrix
+    new_matrix = n*new_matrix/np.sum(new_matrix)
+    return new_matrix
+
+def random_permutation_matrix_no_dig(size_of_perm):
+    # Generate a random permutation of integers from 0 to n-1
+    arr = random.permutation(size_of_perm)
+    while np.any(arr == np.arange(size_of_perm)):
+        arr = random.permutation(size_of_perm)
+
+    # Create an n x n matrix filled with zeros
+    permutation_matrix = np.zeros((size_of_perm, size_of_perm), dtype=int)
+
+    # Place ones according to the permutation
+    for i in range(size_of_perm):
+        permutation_matrix[i, arr[i]] = 1
+
+    return permutation_matrix
+
+n=64
+start_time = time.time()
+perm_matrix = traffic_generator(100,20,0.5,n,0.01)
+end_time = time.time()
+elapsed_time = end_time - start_time
+print(f"Elapsed time: {elapsed_time} seconds")
+
+#print(perm_matrix)
+#for i in range(n):
+   # print(np.sum(perm_matrix[i]))
+
